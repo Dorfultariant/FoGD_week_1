c058790a8c02f1c5ad316fae0017d289798d9d30
Dorfultariant <tunxero@hotmail.com>
2025-06-09 18:57:54 +0300

    Physics process to process (somethings effy here)

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -4,10 +4,12 @@ extends Node
 const SPEED : int = 500
 
 @onready var ICON : Sprite2D = $Icon
-	
-var vel = Vector2.ZERO
 
 # Task 3,4,5 Sprite2D movement with vector normalization and movement speed related to game running speed
-func _physics_process(delta: float) -> void:
+func _process(delta: float) -> void:
 	var inp_vec = Input.get_vector("left","right", "up", "down").normalized()
 	ICON.position += inp_vec * SPEED * delta
+
+#func _physics_process(delta: float) -> void:
+#	var inp_vec = Input.get_vector("left","right", "up", "down").normalized()
+#	ICON.position += inp_vec * SPEED * delta
